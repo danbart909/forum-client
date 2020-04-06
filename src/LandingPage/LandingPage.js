04df13be5b31b2fc2c15e86743854a0522c2b578
Dan Bartlett <danbart909@gmail.com>
2020-04-05 21:28:06 -0400

    landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react'
 export default class LandingPage extends Component {
   render() {
     return (
-      <div>
-        Landing Page
-      </div>
+      <>
+        <header role="banner">
+            <h1>Thinkful Forum Landing Page</h1>
+        </header>
+        <section id='s-landing'>
+          <div id='div-landing-info'>
+            <p>Welcome to the Thinkful Forum, envisioned an alternate means of communication between students in my cohort.</p>
+            <p>Use the '<span id='forum'>Forum</span>' link in the navbar to access the main forum from anywhere. '<span id='homepage'>Homepage</span>' will link back to this page.</p>
+            <p>The usernames <span id='batman'>Batman</span>, <span id='superman'>Superman</span>, and <span id='lexluthor'>Lex Luthor</span> and all open to guests. The password to these users is simply '<span id='password'>password</span>'.</p>
+            <p>You may register with your own username and password and post at your leisure.</p>
+          </div>
+          <div id='div-landing-details'>
+            <p><a href='https://github.com/danbart909/forum-client'>Client Repository at Github</a></p>
+            <p><a href='https://github.com/danbart909/forum-server'>Server Repository at Github</a></p>
+          </div>
+        </section>
+      </>
     )
   }
 }
