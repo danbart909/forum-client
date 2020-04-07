d6a33010c38f7b4d15133121797ede3b9db0a219
Dan Bartlett <danbart909@gmail.com>
2020-04-07 18:50:58 -0400

    modify info

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -11,6 +11,7 @@ export default class LandingPage extends Component {
           <div id='div-landing-info'>
             <p>Welcome to the Thinkful Forum, envisioned an alternate means of communication between students in my cohort.</p>
             <p>Please give the Forum Overview page (/forum) ample time to pull data from the server the first time it loads.</p>
+            <p>Threads in the Forum Overview are ordered by timestamp of last reply, meaning threads with more recent posts will be at the top (posting a reply in a thread will move that thread to the top of the forum list). Replies in the Thread Overview are listed chronologically.</p>
             <p>Use the '<span id='forum'>Forum</span>' link in the navbar to access the main forum from anywhere. '<span id='homepage'>Homepage</span>' will link back to this page.</p>
             <p>The usernames <span id='batman'>Batman</span>, <span id='superman'>Superman</span>, and <span id='lexluthor'>Lex Luthor</span> are all open to guests. The password to these users is simply '<span id='password'>password</span>'.</p>
             <p>The current design was made with mobile in mind and as a result it is, for now, better viewed with a smaller screen.</p>
