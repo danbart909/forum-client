7bea964c807e78c0e089c4a40b0b06e125e822bc
Dan Bartlett <danbart909@gmail.com>
2020-04-07 18:37:24 -0400

    update

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -10,6 +10,7 @@ export default class LandingPage extends Component {
         <section id='s-landing'>
           <div id='div-landing-info'>
             <p>Welcome to the Thinkful Forum, envisioned an alternate means of communication between students in my cohort.</p>
+            <p>Please give the Forum Overview page (/forum) ample time to pull data from the server the first time it loads.</p>
             <p>Use the '<span id='forum'>Forum</span>' link in the navbar to access the main forum from anywhere. '<span id='homepage'>Homepage</span>' will link back to this page.</p>
             <p>The usernames <span id='batman'>Batman</span>, <span id='superman'>Superman</span>, and <span id='lexluthor'>Lex Luthor</span> are all open to guests. The password to these users is simply '<span id='password'>password</span>'.</p>
             <p>The current design was made with mobile in mind and as a result it is, for now, better viewed with a smaller screen.</p>
